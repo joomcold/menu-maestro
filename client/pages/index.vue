89f982d904ffec074a53a702d3fa6d70136863a8
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-10-12 22:49:07 +0700

    Finish CI

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -12,7 +12,11 @@
           </ColorScheme>
         </div>
       </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">
+      <UButton
+        icon="i-heroicons-book-open"
+        to="https://ui.nuxt.com"
+        target="_blank"
+      >
         Open Nuxt UI Documentation
       </UButton>
     </UCard>
