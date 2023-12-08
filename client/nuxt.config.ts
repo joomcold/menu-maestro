e25e6c588ec190812ff627e16d6639b58fd8ae20
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-12-08 11:40:28 +0700

    Add dependencies

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  modules: ['@nuxt/ui'],
   devtools: { enabled: true },
-  devServer: { port: 5173 }
+  devServer: { port: 5173 },
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/content', '@vee-validate/nuxt']
 })
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Index {{ message }}</h1>
+    <ContentDoc />
   </div>
 </template>
 
