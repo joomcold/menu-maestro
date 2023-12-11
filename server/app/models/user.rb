6403f54cc854816b4bd37a7971cb1e2a5a737daf
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-12-11 17:14:29 +0700

    Add authentication

---
+++ server/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :trackable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: self
+end
