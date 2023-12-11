0a46a5fbbaab020d65aec5ac50decde1158d978a
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-12-11 17:37:37 +0700

    Add sample a spec

--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe(User) do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to(validate_presence_of(:email)) }
+    it { is_expected.to(validate_presence_of(:password)) }
+    it { is_expected.to(validate_length_of(:password).is_at_least(6)) }
+  end
+
+  # describe 'associations' do
+  #   it { is_expected.to(have_many(:orders)) }
+  # end
 end
