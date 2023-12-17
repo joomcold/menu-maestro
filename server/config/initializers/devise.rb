50d2c242825906d527869ddd6267bfec76b0b923
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-12-17 08:40:02 +0700

    Update path for Auth

--- server/config/initializers/devise.rb
+++ server/config/initializers/devise.rb
@@ -19,10 +19,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
     jwt.dispatch_requests = [
-      ['POST', %r{^/users/login$}]
+      ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/users/logout$}]
+      ['DELETE', %r{^/logout$}]
     ]
     jwt.expiration_time = 180.minutes.to_i
   end
